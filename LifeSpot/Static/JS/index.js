0bc33871b137fd3044f0807b43d91dae5aab1367
kema1989 <86516029+kema1989@users.noreply.github.com>
2022-04-08 00:22:01 +0600

    Не проверяйте пока😥, мне там еще надо доделать((

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,10 @@
+﻿//let age = prompt('Введите ваш возраст')
+//if (age < 18) {
+//    alert("Сайт не для малышей!")
+//    window.location.href = "http://www.google.com"
+//}
+//else {
+//    alert("Добро пожаловать на Lifespot!")
+//}
+
+let contentFilter = 
