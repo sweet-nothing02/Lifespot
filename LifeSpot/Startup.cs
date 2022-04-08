244585c05b3bb14816711fb28e9758314ab27dff
kema1989 <86516029+kema1989@users.noreply.github.com>
2022-04-08 17:53:40 +0600

    .

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -51,6 +51,17 @@ namespace LifeSpot
                     await context.Response.WriteAsync(html.ToString());
                 });
 
+                endpoints.MapGet("/about", async context =>
+                {
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+
+                    await context.Response.WriteAsync(html.ToString());
+                });
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -13,29 +13,31 @@ function filterContent( inputParseFunction ) {
     }
 }
 
-// 29.1.2
-// Проверка на возраст
+//
+
+let session = new Map();
+
+let startDate = new Date().toLocaleString();
+
 function handleSession() {
-    let session = new Map();
+    session.set("startDate", new Date().toLocaleString())
 
     session.set("userAgent", window.navigator.userAgent)
+}
 
+function checkAge() {
     session.set("age", prompt("Пожалуйста, введите ваш возраст"))
 
     if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-
         alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
         session.set("startDate", startDate)
     } else {
         alert("Наши трансляции не предназначены ля лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-
-    return session;
 }
 
-function logSession(session) {
+let sessionLog = function logSession() {
     for (let result of session) {
         console.log(result)
     }
